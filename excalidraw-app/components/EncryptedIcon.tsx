76e292c67880f7930935f7b60527eff7428c338d
keerthana <keerthanacs2506@gmail.com>
2026-09-08 22:22:58 +0530

    Update Colearn branding and remove external links

--- excalidraw-app/components/EncryptedIcon.tsx
+++ excalidraw-app/components/EncryptedIcon.tsx
@@ -8,7 +8,7 @@ export const EncryptedIcon = () => {
   return (
     <a
       className="encrypted-icon tooltip"
-      href="https://plus.excalidraw.com/blog/end-to-end-encryption"
+      href="#"
       target="_blank"
       rel="noopener"
       aria-label={t("encrypted.link")}
